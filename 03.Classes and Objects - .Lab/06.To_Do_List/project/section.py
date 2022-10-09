da5fa1eb878e69af63487d82c5a0db5bc4381d36
ChristiyanGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-10-09 17:19:51 +0300

    05.To_do

---
+++ 03.Classes and Objects - .Lab/06.To_Do_List/project/section.py
@@ -0,0 +1,37 @@
+from project.task import Task
+
+
+class Section:
+    def __init__(self, name):
+        self.name = name
+        self.tasks = []
+
+    def add_task(self, task: Task):
+        for section_task in self.tasks:
+            if section_task.name == task.name:
+                return f"Task is already in the section {self.name}"
+        self.tasks.append(task)
+        return f"Task {task.details()} is added to the section"
+
+    def complete_task(self, task_name):
+        for task in self.tasks:
+            if task.name == task_name:
+                task.completed = True
+                return f"Completed task {task_name}"
+        return f'Could not find task with the name {task_name}'
+
+    def clean_section(self):
+        removed = 0
+        for section_task in self.tasks:
+            if section_task.completed:
+                self.tasks.remove(section_task)
+                removed +=1
+        return f"Cleared {removed} tasks."
+
+    def view_section(self):
+        result = f"Section {self.name}:"
+        for task in self.tasks:
+            result += '\n' + task.details()
+        return result
+
+
