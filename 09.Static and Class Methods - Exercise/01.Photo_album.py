d76cb24152819948a6d62e9e8d0b6a6b616408bb
ChristiyanGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-11-02 21:02:02 +0200

    Create 01.Photo_album.py

---
+++ 09.Static and Class Methods - Exercise/01.Photo_album.py
@@ -0,0 +1,52 @@
+from math import  ceil
+class PhotoAlbum:
+    MAX_PHOTO_PER_PAGE = 4
+    def __init__(self,pages):
+        self.pages = pages
+        self.photos = self.create_matrix(self.pages)
+
+
+    def create_matrix(self,pages):
+        matrix = []
+        for _ in range(pages):
+            matrix.append([])
+        return matrix
+
+    @classmethod
+    def from_photos_count(cls, photos_count):
+        result = ceil(photos_count / PhotoAlbum.MAX_PHOTO_PER_PAGE)
+        return cls(result)
+
+    def add_photo(self, label):
+        for idx, page in enumerate(self.photos):
+            if len(page) < PhotoAlbum.MAX_PHOTO_PER_PAGE:
+                page.append(label)
+                return f"{label} photo added successfully on page {idx+1} slot {len(page)}"
+        return "No more free slots"
+
+    def display(self):
+        sep = "-" * 11
+        result = sep + "\n"
+        for page in self.photos:
+            result += ' '.join(["[]" for _ in page]) + '\n'
+            result += sep + '\n'
+
+
+        return result
+
+
+
+
+
+
+
+
+album = PhotoAlbum(2)
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+print(album.display())
